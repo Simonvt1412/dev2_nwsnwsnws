ed26af3f91cd3db6f103eddd7bdf4b3d349a3a1c
Simon Van Tomme <simon.vantomme@student.arteveldehs.be>
2025-03-06 15:12:30 +0100

    het werkt half

--- server/routes.ts
+++ server/routes.ts
@@ -14,7 +14,7 @@ router.get("/", (req: Request, res: Response) => {
 router.get("/news", async (req: Request, res: Response) => {
     const news: News[] = await getAllNews();
   
-    res.render("index", { news, title: "Recent nieuws" });
+    res.render("news", { news, title: "Recent nieuws" });
   });
 
 export default router;
